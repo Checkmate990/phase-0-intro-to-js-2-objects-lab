a9de74954c181ff9e5d0b756a2d40ea6623e1a2d
Checkmate990 <108345290+Checkmate990@users.noreply.github.com>
2023-01-11 18:22:41 -0700

    Update index.js
    Objects

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+const employee = {
+    name: 'John',
+    streetAddress: 9227,
+}
+
+function updateEmployeeWithKeyAndValue(){
+    const employeeb= {...employee};
+    employeeb.name = 'Sam',
+    employeeb.streetAddress = '11 Broadway'
+    return employeeb;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(){
+    employee.name = 'Sam',
+    employee.streetAddress = '12 Broadway'
+    return employee;
+}
+
+function deleteFromEmployeeByKey(){
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(){
+    delete employee.name;
+    const newEmployee = {...employee};
+    return employee
+    return newEmployee
+}
+
